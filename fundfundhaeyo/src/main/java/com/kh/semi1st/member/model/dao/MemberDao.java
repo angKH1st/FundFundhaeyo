ef518554373e72bba3bd0244d5b8c5ce2ba73254
Jaeyeong Kim <kjyyy7341@gmail.com>
2024-02-16 07:31:26 +0900

    회원가입 dao 추가
    회원가입 dao 추가

--- fundfundhaeyo/src/main/java/com/kh/semi1st/member/model/dao/MemberDao.java
+++ fundfundhaeyo/src/main/java/com/kh/semi1st/member/model/dao/MemberDao.java
@@ -25,6 +25,12 @@ public class MemberDao {
 		}
 	}
 	
+	/** 아이디와 비밀번호를 입력받아 로그인을 처리하는 메소드
+	 * @param conn
+	 * @param userId
+	 * @param userPwd
+	 * @return
+	 */
 	public Member loginMember(Connection conn, String userId, String userPwd) {
 		Member m = null;
 		PreparedStatement pstmt = null;
@@ -66,5 +72,38 @@ public class MemberDao {
 		}
 		
 		return m;
+	}
+
+	/** 회원가입을 위해 입력한 정보들로 멤버 객체를 받아 가입을 처리하는 메소드
+	 * @param conn
+	 * @param m
+	 * @return
+	 */
+	public int insertMember(Connection conn, Member m) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("insertMember");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, m.getUserId());
+			pstmt.setString(2, m.getUserPwd());
+			pstmt.setString(3, m.getUserName());
+			pstmt.setString(4, m.getUserSsn());
+			pstmt.setString(5, m.getNickname());
+			pstmt.setString(6, m.getPhone());
+			pstmt.setString(7, m.getEmail());
+			pstmt.setString(8, m.getAddress());
+			pstmt.setString(9, m.getMarketing());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
 	}
 }
