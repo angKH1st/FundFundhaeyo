e3b8bfc09eb67119b94e2fd040bdbb3a786be2a8
김재영 <kjyyy7341@gmail.com>
2024-02-29 17:04:32 +0900

    add memberDeleteLogic

---
+++ fundfundhaeyo/src/main/java/com/kh/semi1st/member/controller/MemberDeleteController.java
@@ -0,0 +1,53 @@
+package com.kh.semi1st.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.semi1st.member.model.service.MemberService;
+
+/**
+ * Servlet implementation class MemberDeleteController
+ */
+@WebServlet("/delete.me")
+public class MemberDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
+		
+		int result = new MemberService().deleteMember(userId, userPwd);		
+		
+		if(result > 0) {
+			HttpSession session = request.getSession();
+			session.removeAttribute("loginUser");
+		}
+		
+		response.getWriter().print(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
